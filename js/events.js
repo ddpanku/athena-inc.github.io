de88316a98dea6d14535b211cb92815fd1c87094
Smruti Ranjan Rana <30388973+smrutiranjanrana@users.noreply.github.com>
2018-09-13 09:42:53 +0530

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,7 +1,7 @@
 (function ($) {
     'use restrict'
 
-    var isRegdOpen = true;
+    var isRegdOpen = false;
     var eventDate = "24"; // Date
     var eventMonth = "Sep"; // Month
     var eventYear = "2018"; //Year
